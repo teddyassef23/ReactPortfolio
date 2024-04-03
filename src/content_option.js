69b101283b5db96bae0e7b407773f52aa3718bfc
Tewodros Assefa <teddyassef23@gmail.com>
2024-04-03 17:31:04 -0500

    almost done

---
+++ src/content_option.js
@@ -0,0 +1,169 @@
+// const img = require('./../public/img')
+const logotext = "TEDDY";
+const meta = {
+    title: "Teddy Assefa",
+    description: "I’m Infrastructure Admin and Full stack developer.",
+};
+
+const introdata = {
+    title: "I’m Teddy Assefa",
+    animated: {
+        first: "I am full-Sack Developer",
+        second: "I am CISCO network Developer",
+        third: "I am windows System Admin",
+        fourth: "I am Infrastructure Admin",
+        fives : "I am Integration developer",
+    },
+    description: "Proven full-stack java developer with a passion to learn and work with new technologies that make a positive impact on society.  proficient in Angular, Java and JavaScript develop and improve performance. Dedicated to learning better ways to write clean and efficient code while keeping up with the newest tech stacks, improve my creativity, analytics, and problem-solving while providing opportunities to grow and enhance my technical abilities.",
+    your_img_url: "https://media.istockphoto.com/id/1143736474/photo/dark-web-hooded-hacker.jpg?s=2048x2048&w=is&k=20&c=5ZaTronEaRMH2MAbPUfsKQDIXo14GeSnugNM8rvNQ4M=",
+};
+
+const dataabout = {
+    title: "Smeary of My carrara path",
+    aboutme: "As an organized and highly creative individual with twelve (12) years of system and Networking Administration experience; love the challenges of working with a dynamic and fast paced working environment. The knowledge gained from my corporate experience has shaped my ability to adapt quickly, work efficiently, think creatively, and work effectively as part of a team or on my own.",
+};
+const worktimeline = [
+
+
+
+    {
+        jobtitle: "Full Stack web Developer",
+        where: "AWG",
+        date: "2020",
+    },
+    {
+        jobtitle: "Middle ware integration Developer",
+        where: "DXC Technology",
+        date: "2017",
+        Side: "Developer",
+    },
+    {
+        jobtitle: "Microsoft Windows Server ",
+        where: "Dashen Bank ",
+        date: "2009",
+        Side: "Sys Admin",
+    },
+
+];
+
+const skills = [
+
+    {
+        name: "Angular, React, HTML & CSS",
+        value: 90,
+    },
+    {
+        name: "Java core, Spring boot",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "Microsoft Sever Admin",
+        value: 90,
+    },
+    {
+        name: "CISCO Network Administration",
+        value: 75,
+    },
+    {
+        name: "CISCO Network switching and Router contactConfig",
+        value: 70,
+    },
+    {
+        name: "Could Verctualzetion ",
+        value: 60,
+    },
+    {
+        name: "Help dest support ",
+        value: 85,
+    },
+];
+
+const services = [
+    {
+        title: "WebSite Developer",
+        description: "Proven full-stack java developer with a passion to learn and work with new technologies that make a positive impact on society. Extremely proficient in Angular, Java and JavaScript develop and improve performance. Dedicated to learning better ways to write clean and efficient code while keeping up with the newest tech stacks, improve my creativity, analytics, and problem-solving while providing opportunities to grow and enhance my technical abilities",
+    },
+    {
+        title: "integration Developer",
+        description: "Experienced in DataPower, MQ, IIB/WMB, WebSphere Application Server administration. Experienced in message flow development in WMB/IIB using various nodes and in WebSphere MQ, WebSphere Message Broker, WebSphere Application Server infrastructure design and installation.",
+    },
+    {
+        title: "Infrastructure Administration",
+        description: "As an organized and highly creative individual with more than six (6) years of system and Networking Administration experience; love the challenges of working with a dynamic and fast paced working environment. The knowledge gained from my corporate experience has shaped my ability to adapt quickly, work efficiently, think creatively, and work effectively as part of a team or on my own.",
+    },
+];
+
+const dataportfolio = [
+    {
+    img: "./img/questions.jpg",
+    description: "The questions appleton is developed for University of Minnesota class home work.",
+    link: "#",
+},
+{
+img: "./img/cultural_foods.jpg",
+description: "cultural_foods appleton is developed for University of Minnesota project.",
+link: "#",
+},
+    {
+    img: "./img/medication.jpg",
+    description: "The medication or Pull management  appleton is developed for University of Minnesota project work.",
+    link: "#",
+},
+    {
+    img: "./img/weather.jpg",
+    description: "weather forecasting  appleton is developed for University of Minnesota class home work..",
+    link: "#",
+},
+    {
+    img: "./img/password.jpg",
+    description: "Password Generator  appleton is developed for University of Minnesota class home work.",
+    link: "#",
+},
+//     {
+//     img: "./img/MicrosoftServer.jpg",
+//     description: "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+// },
+// {
+//     img: "https://picsum.photos/400/800/?grayscale",
+//     description: "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+// },
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(123)456-7878",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/dashboard",
+    facebook: "https://facebook.com",
+    linkedin: "www.linkedin.com/in/tedddy",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
